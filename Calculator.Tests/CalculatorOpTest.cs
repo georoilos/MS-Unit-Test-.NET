81b760acdddba3326971c352cbf25d8bb907e3d5
George Roilos <georoilos@yahoo.com>
2017-08-22 23:47:15 +0100

    MS Test Template
    MS Unit Test Template

---
+++ Calculator.Tests/CalculatorOpTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Calculator.Tests
+{	[TestClass]
+	public class CalculatorOpTest
+	{	[TestMethod]
+		public void InputValues1()
+		{	//Arrange
+			CalculatorOp calc = new CalculatorOp();
+			//Act
+			int act = calc.Add(5, 4);
+			//Assert
+			Assert.AreEqual(9, act);
+		}
+		[TestMethod]
+		public void InputValues2()
+		{	//Arrange
+			CalculatorOp calc = new CalculatorOp();
+			//Act
+			int act = calc.Mul(5, 4);
+			//Assert
+			Assert.AreEqual(20, act);
+		}
+	}
+}
---
+++ Calculator/CalculatorOp.cs
@@ -0,0 +1,8 @@
+﻿namespace Calculator
+{
+	public class CalculatorOp
+	{
+		public int Add(int num1, int num2) => num1 + num2;
+		public int Mul(int num1, int num2) => num1 * num2;
+	}
+}
